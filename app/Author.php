34b97215a2462b2a5aef0ba912e11184d817edaf
pisey <pisey@marinax.biz>
2019-01-22 14:06:41 +0700

    first commit

---
+++ app/Author.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+}
